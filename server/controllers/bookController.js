ad0252ad1fcd2e75121e42981f9fcfb2493ec472
JakeMackBrown <JakeMackBrown@gmail.com>
2024-10-18 12:54:06 -0500

    CRUD + starting on routes

---
+++ server/controllers/bookController.js
@@ -0,0 +1,67 @@
+const Book = require('../models/book')
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+    res.json(books)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const book = await Book.findById(id)
+    if (book) {
+      return res.json(book)
+    }
+    return res.status(404).send('Book with the specified ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const createBook = async (req, res) => {
+  try {
+    const book = new Book(req.body)
+    await book.save()
+    return res.status(201).json({ book })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params
+    const book = await Book.findByIdAndUpdate(id, req.body, { new: true })
+    if (book) {
+      return res.status(200).json(book)
+    }
+    throw new Error('Book not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Book.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Book deleted')
+    }
+    throw new Error('Book not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook
+}
---
+++ server/controllers/meetingController.js
@@ -0,0 +1,67 @@
+const Meeting = require('../models/meeting')
+
+const getAllMeetings = async (req, res) => {
+  try {
+    const meetings = await Meeting.find()
+    res.json(meetings)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getMeetingById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const meeting = await Meeting.findById(id)
+    if (meeting) {
+      return res.json(meeting)
+    }
+    return res.status(404).send('Meeting with the specified ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const createMeeting = async (req, res) => {
+  try {
+    const meeting = new Meeting(req.body)
+    await meeting.save()
+    return res.status(201).json({ meeting })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const updateMeeting = async (req, res) => {
+  try {
+    const { id } = req.params
+    const meeting = await Meeting.findByIdAndUpdate(id, req.body, { new: true })
+    if (meeting) {
+      return res.status(200).json(meeting)
+    }
+    throw new Error('Meeting not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const deleteMeeting = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Meeting.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Meeting deleted')
+    }
+    throw new Error('Meeting not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  getAllMeetings,
+  getMeetingById,
+  createMeeting,
+  updateMeeting,
+  deleteMeeting
+}
