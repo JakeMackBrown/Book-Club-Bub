c1dafe313fa846441126bbfd3b4b08303a06e410
JakeMackBrown <JakeMackBrown@gmail.com>
2024-10-24 11:44:32 -0500

    fixed some displayed data, committing before adding more users and discussions to seeds in case something goes wrong

--- client/meetings.js
+++ client/meetings.js
@@ -4,8 +4,9 @@ async function showData (){
     console.log(response)
     const meetingsContainer = document.getElementById('meetings-container');
     response.data.forEach(meeting => {
+      console.log("Book ID:", meeting.book_id)
       const meetingItem = document.createElement('div');
-      meetingItem.textContent = `Meeting on ${meeting.date} at ${meeting.location}, Book ID: ${meeting.book_id}`;
+      meetingItem.textContent = `Meeting on ${new Date(meeting.date).toDateString()} at ${meeting.location}, Book: ${meeting.book_id.title}`;
       meetingsContainer.appendChild(meetingItem);
       });
 
@@ -13,7 +14,7 @@ async function showData (){
       const discussionsContainer = document.getElementById('discussions-container');
       discussionsResponse.data.forEach(discussion => {
         const discussionItem = document.createElement('div');
-        discussionItem.textContent = `User: ${discussion.user_id}, Date: ${discussion.date}, Content: ${discussion.content}`;
+        discussionItem.textContent = `User: ${discussion.user_id}, Date: ${new Date(discussion.date).toDateString()}, Content: ${discussion.content}`;
         discussionsContainer.appendChild(discussionItem);
       });
 
