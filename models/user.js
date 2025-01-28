2db56359c425c2b9015be71c9dee8d84f081614b
JakeMackBrown <JakeMackBrown@gmail.com>
2025-01-27 19:25:41 -0600

    Updated user model to include password hashing and comparison

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
@@ -21,4 +22,15 @@ const userSchema = new Schema({
   }
 }, { timestamps: true })
 
-module.exports = userSchema
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema)
